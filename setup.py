3939696ded00bb0521901e21f913c62da6c37a7a
dugsong <dugsong@0225ec8c-8f27-0410-80df-577ead3473a2>
2004-09-06 21:54:32 +0000

    better support for non-BSD platforms
    git-svn-id: http://pypcap.googlecode.com/svn/trunk@11 0225ec8c-8f27-0410-80df-577ead3473a2

--- setup.py
+++ setup.py
@@ -3,20 +3,40 @@
 # $Id$
 
 from distutils.core import setup, Extension
-import sys
+import glob, os, sys
 
-if 'bsd' in sys.platform:
-    pcap_incdir = []
-    pcap_libdir = []
+pcap_incdir = pcap_libdir = []
+pcap_libs = []
+
+# Try standard installation paths.
+if os.path.exists('/usr/include/pcap.h'):
+    pcap_libs = [ 'pcap' ]
+elif os.path.exists('/usr/include/pcap/pcap.h'):
+    pcap_incdir = [ '/usr/include/pcap' ]
+    pcap_libs = [ 'pcap' ]
 else:
-    pcap_incdir = [ '%s/include' % sys.prefix ]
-    pcap_libdir = [ '%s/lib' % sys.prefix ]
+    # Try common installation paths.
+    for prefix in [ sys.prefix ] + glob.glob('/opt/libpcap*'):
+        if glob.glob('%s/include/pcap*' % prefix):
+            if os.path.exists('%s/include/pcap/pcap.h'):
+                pcap_incdir = [ '%s/include/pcap' % prefix ]
+            else:
+                pcap_incdir = [ '%s/include' % prefix ]
+            pcap_libdir = [ '%s/lib' % prefix ]
+            if os.path.exists('%s/lib/libwpcap.a' % prefix):
+                pcap_libs = [ 'wpcap' ]
+            else:
+                pcap_libs = [ 'pcap' ]
+            break
+
+if not pcap_libs:
+    raise "couldn't find installed libpcap"
 
 pcap = Extension(name='pcap',
                  sources=[ 'pcap.c' ],
                  include_dirs=pcap_incdir,
                  library_dirs=pcap_libdir,
-                 libraries=[ 'pcap' ])
+                 libraries=pcap_libs)
 
 setup(name='pcap',
       version='0.2',
