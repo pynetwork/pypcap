ec1f426ce2eec5e1c679a06ebd4e3299d858e608
dugsong <dugsong@0225ec8c-8f27-0410-80df-577ead3473a2>
2005-01-26 18:32:30 +0000

    automated unit tests
    git-svn-id: http://pypcap.googlecode.com/svn/trunk@34 0225ec8c-8f27-0410-80df-577ead3473a2

--- test.py
+++ test.py
@@ -1,32 +1,52 @@
 #!/usr/bin/env python
 
-import getopt, glob, time, sys
-sys.path.insert(0, glob.glob('build/lib.*'))
+import glob, sys, unittest
+sys.path.insert(0, glob.glob('build/lib.*')[0])
 import pcap
 
-def print_pkt(ts, pkt, arg):
-    print '%s: %d bytes' % (time.ctime(ts), len(pkt))
+class PcapTestCase(unittest.TestCase):
+    def test_pcap_iter(self):
+        l = [ (x[0], len(x[1])) for x in pcap.pcap('test.pcap') ]
+        assert l == [(1092256609.9265549, 62), (1092256609.9265759, 54), (1092256610.332396, 62), (1092256610.3324161, 54), (1092256610.8330729, 62), (1092256610.8330951, 54)], 'pcap iter'
 
-def usage():
-    print >>sys.stderr, 'usage: %s [-i device] [pattern]' % sys.argv[0]
-    sys.exit(1)
+    def test_pcap_properties(self):
+        p = pcap.pcap('test.pcap')
+        assert (p.name, p.snaplen, p.dloff, p.filter) == ('test.pcap', 2000, 14, ''), 'pcap properties'
 
-def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'i:h')
-    name = None
-    for o, a in opts:
-        if o == '-i': name = a
-        else: usage()
-    
-    pc = pcap.pcap(name)
-    pc.setfilter(' '.join(args))
-    try:
-        print 'listening on %s: %s' % (pc.name, pc.filter)
-        pc.loop(print_pkt)
-    except KeyboardInterrupt:
-        nrecv, ndrop, nifdrop = pc.stats()
-        print '\n%d packets received by filter' % nrecv
-        print '%d packets dropped by kernel' % ndrop
+    def test_pcap_errors(self):
+        p = pcap.pcap('test.pcap')
+        stats_err = "Statistics aren't available from savefiles"
+        try:
+            print p.stats()
+        except OSError, msg:
+            assert msg != stats_err, 'pcap_stats'
+        assert p.geterr() == stats_err, 'pcap_geterr'
+
+    def __test_pcap_cb(self, method):
+        def __cnt_handler(ts, pkt, d):
+            d['cnt'] += 1
+        p = pcap.pcap('test.pcap')
+        d = { 'cnt':0 }
+        n = getattr(p, method)(__cnt_handler, d)
+        if method == 'dispatch': assert n == 0
+        assert d['cnt'] == 6
+
+        def __bad_handler(ts, pkt, arg):
+            raise NotImplementedError
+        p = pcap.pcap('test.pcap')
+        try:
+            getattr(p, method)(__bad_handler)
+        except NotImplementedError:
+            pass
+
+    def test_pcap_dispatch(self):
+        self.__test_pcap_cb('dispatch')
+        
+    def test_pcap_loop(self):
+        self.__test_pcap_cb('loop')
+        
+    def test_pcap_readpkts(self):
+        assert len(pcap.pcap('test.pcap').readpkts()) == 6
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
