3f82cab7c7b068f475e95b35713bc544f969cbbe
dugsong <dugsong@0225ec8c-8f27-0410-80df-577ead3473a2>
2005-01-26 21:39:20 +0000

    use config.h for pcap_file(), support Python < 2.3
    git-svn-id: http://pypcap.googlecode.com/svn/trunk@45 0225ec8c-8f27-0410-80df-577ead3473a2

--- pcap_ex.c
+++ pcap_ex.c
@@ -11,16 +11,30 @@
 #endif
 
 #include <pcap.h>
-#ifndef _WIN32
+#ifdef HAVE_PCAP_INT_H
 # include <pcap-int.h>
 #endif
 #include "pcap_ex.h"
+
+#include "config.h"
+
+/* XXX - hack around older Python versions */
+#include "patchlevel.h"
+#if PY_VERSION_HEX < 0x02030000
+int    PyGILState_Ensure() { return (0); }
+void   PyGILState_Release(int gil) { }
+#endif
 
 void
 pcap_ex_immediate(pcap_t *pcap)
 {
 #ifdef BIOCIMMEDIATE
-	ioctl(pcap_fileno(pcap), BIOCIMMEDIATE, 1);
+#ifdef HAVE_PCAP_FILE
+	if (pcap_file(pcap) == NULL)
+#else
+        if (pcap->sf.rfile == NULL)
+#endif
+		ioctl(pcap_fileno(pcap), BIOCIMMEDIATE, 1);
 #endif
 }
 
@@ -66,11 +80,15 @@ pcap_ex_fileno(pcap_t *pcap)
 #ifdef _WIN32
 	/* XXX - how to handle savefiles? */
 	return ((int)pcap_getevent(pcap));
-#else
-	if (pcap->sf.rfile != NULL)
-		return (fileno(pcap->sf.rfile));
-	return (pcap_fileno(pcap));
 #endif
+#ifdef HAVE_PCAP_FILE
+	FILE *f = pcap_file(pcap);
+#else
+	FILE *f = pcap->sf.rfile;
+#endif
+	if (f != NULL)
+		return (fileno(f));
+	return (pcap_fileno(pcap));
 }
 
 static int __pcap_ex_gotsig;
@@ -131,7 +149,11 @@ pcap_ex_next(pcap_t *pcap, struct pcap_pkthdr **hdr, u_char **pkt)
 			return (-1);
 		}
 		if ((__pkt = (u_char *)pcap_next(pcap, &__hdr)) == NULL) {
+#ifdef HAVE_PCAP_FILE
+			if (pcap_file(pcap) != NULL)
+#else
 			if (pcap->sf.rfile != NULL)
+#endif
 				return (-2);
 			FD_ZERO(&rfds);
 			FD_SET(fd, &rfds);
