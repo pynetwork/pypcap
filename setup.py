4dd51bbd19ed18c596fd140e6e59e8bcf0871717
Arturo Filastò <art@fuffa.org>
2012-12-06 21:17:12 +0100

    Add support for finding pcap libraries on Ubuntu 32bit, 64 bit and Mac OS X 10.7, 10.8. * Refactor setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 #
 # $Id$
 
+from __future__ import with_statement
 from distutils.core import setup, Extension
 from distutils.command import config, clean
 import cPickle, glob, os, sys
@@ -9,11 +10,30 @@ import cPickle, glob, os, sys
 pcap_config = {}
 pcap_cache = 'config.pkl'
 
+class FileNotFound(Exception):
+    pass
+
+def recursive_search_dirs(dirs, target_files):
+    for d in dirs:
+        #print "searching %s" % d
+        r = recursive_search(d, target_files)
+        if r:
+            return r
+    raise FileNotFound(target_files)
+
+def recursive_search(path, target_files):
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            #print "Searching for %s %s" % (root, filename)
+            if filename in target_files:
+                return os.path.join(root, filename)
+    return None
+
 class config_pcap(config.config):
     description = 'configure pcap paths'
     user_options = [ ('with-pcap=', None,
                       'path to pcap build or installation directory') ]
-    
+
     def initialize_options(self):
         config.config.initialize_options(self)
         self.dump_source = 0
@@ -23,51 +43,85 @@ class config_pcap(config.config):
     def _write_config_h(self, cfg):
         # XXX - write out config.h for pcap_ex.c
         d = {}
-        if os.path.exists(os.path.join(cfg['include_dirs'][0], 'pcap-int.h')):
-            d['HAVE_PCAP_INT_H'] = 1
-        buf = open(os.path.join(cfg['include_dirs'][0], 'pcap.h')).read()
-        if buf.find('pcap_file(') != -1:
-            d['HAVE_PCAP_FILE'] = 1
-        if buf.find('pcap_compile_nopcap(') != -1:
-            d['HAVE_PCAP_COMPILE_NOPCAP'] = 1
-        if buf.find('pcap_setnonblock(') != -1:
-            d['HAVE_PCAP_SETNONBLOCK'] = 1
+        include_dir = os.path.join(cfg['include_dirs'][0])
+        try:
+            with open(os.path.join(include_dir, 'pcap-int.h')) as f:
+                d['HAVE_PCAP_INT_H'] = 1
+        except IOError:
+            print "No pcap-int.h found"
+
+        pcap_h_file = open(os.path.join(include_dir, 'pcap.h')).readlines()
+        for line in pcap_h_file:
+            if 'pcap_file(' in line:
+                print "found pcap_file function"
+                d['HAVE_PCAP_FILE'] = 1
+            if 'pcap_compile_nopcap(' in line:
+                print "found pcap_compile_nopcap function"
+                d['HAVE_PCAP_COMPILE_NOPCAP'] = 1
+            if 'pcap_setnonblock(' in line:
+                print "found pcap_setnonblock"
+                d['HAVE_PCAP_SETNONBLOCK'] = 1
         f = open('config.h', 'w')
         for k, v in d.iteritems():
             f.write('#define %s %s\n' % (k, v))
-    
+        f.close()
+
     def _pcap_config(self, dirs=[ None ]):
         cfg = {}
         if not dirs[0]:
             dirs = [ '/usr', sys.prefix ] + glob.glob('/opt/libpcap*') + \
-                   glob.glob('../libpcap*') + glob.glob('../wpdpack*')
+                glob.glob('../libpcap*') + glob.glob('../wpdpack*') + \
+                ['/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/',
+                 '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*']
+
         for d in dirs:
-            for sd in ('include/pcap', 'include', ''):
-                incdirs = [ os.path.join(d, sd) ]
-                if os.path.exists(os.path.join(d, sd, 'pcap.h')):
-                    cfg['include_dirs'] = [ os.path.join(d, sd) ]
-                    for sd in ('lib', 'lib64', ''):
-                        for lib in (('pcap', 'libpcap.a'),
-                                    ('pcap', 'libpcap.so'),
-                                    ('pcap', 'libpcap.dylib'),
-                                    ('wpcap', 'wpcap.lib')):
-                            if os.path.exists(os.path.join(d, sd, lib[1])):
-                                cfg['library_dirs'] = [ os.path.join(d, sd) ]
-                                cfg['libraries'] = [ lib[0] ]
-                                if lib[0] == 'wpcap':
-                                    cfg['libraries'].append('iphlpapi')
-                                    cfg['extra_compile_args'] = \
-                                        [ '-DWIN32', '-DWPCAP' ]
-                                print 'found', cfg
-                                self._write_config_h(cfg)
-                                return cfg
+            # This makes sure that we first search inside of $d/usr/include/pcap
+            search_dirs = [os.path.join(d, 'usr', 'include', 'pcap'), d]
+            try:
+                pcap_h = recursive_search_dirs(search_dirs, ['pcap.h'])
+            except:
+                continue
+
+            if pcap_h:
+                print "Found pcap headers in %s" % pcap_h
+
+                cfg['include_dirs'] = [ os.path.dirname(pcap_h) ]
+                lib_sub_dirs = [os.path.join(d, sub_dir) \
+                        for sub_dir in ('lib', 'lib64', \
+                        'lib/x86_64-linux-gnu', 'lib/i386-linux-gnu', '')]
+
+                lib_files = {
+                        'libpcap.a': 'pcap',
+                        'libpcap.so': 'pcap',
+                        'libpcap.dylib': 'pcap',
+                        'wpcap.lib': 'wpcap'
+                }
+                lib_file_path = recursive_search_dirs(lib_sub_dirs, lib_files.keys())
+
+                print "Found libraries in %s" % lib_file_path
+
+                lib_file = os.path.basename(lib_file_path)
+
+                cfg['library_dirs'] = [ os.path.dirname(lib_file_path) ]
+                cfg['libraries'] = [ lib_files[lib_file] ]
+
+                #cfg['libraries'] = [ lib[0] ]
+                if lib_file == 'wpcap.lib':
+                    cfg['libraries'].append('iphlpapi')
+                    cfg['extra_compile_args'] = \
+                        [ '-DWIN32', '-DWPCAP' ]
+
+                self._write_config_h(cfg)
+                return cfg
+
         raise Exception("couldn't find pcap build or installation directory")
-    
+
     def run(self):
         #config.log.set_verbosity(0)
         cPickle.dump(self._pcap_config([ self.with_pcap ]),
                      open(pcap_cache, 'wb'))
         self.temp_files.append(pcap_cache)
+
 
 class clean_pcap(clean.clean):
     def run(self):
