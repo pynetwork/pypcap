bf98f5dd5b3d091f4285e14f76e4a1e9fed867db
Thomas Grainger <tagrain@gmail.com>
2013-01-05 00:47:58 +0000

    clean up setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,18 @@
 from __future__ import with_statement
 from distutils.core import setup, Extension
 from distutils.command import config, clean
-import cPickle, glob, os, sys
+import cPickle
+import glob
+import os
+import sys
 
 pcap_config = {}
 pcap_cache = 'config.pkl'
 
+
 class FileNotFound(Exception):
     pass
+
 
 def recursive_search_dirs(dirs, target_files):
     for d in dirs:
@@ -21,6 +26,7 @@ def recursive_search_dirs(dirs, target_files):
             return r
     raise FileNotFound(target_files)
 
+
 def recursive_search(path, target_files):
     for root, dirs, files in os.walk(path):
         for filename in files:
@@ -29,10 +35,11 @@ def recursive_search(path, target_files):
                 return os.path.join(root, filename)
     return None
 
+
 class config_pcap(config.config):
     description = 'configure pcap paths'
-    user_options = [ ('with-pcap=', None,
-                      'path to pcap build or installation directory') ]
+    user_options = [('with-pcap=', None,
+                      'path to pcap build or installation directory')]
 
     def initialize_options(self):
         config.config.initialize_options(self)
@@ -66,10 +73,10 @@ class config_pcap(config.config):
             f.write('#define %s %s\n' % (k, v))
         f.close()
 
-    def _pcap_config(self, dirs=[ None ]):
+    def pcap_config(self, dirs=[None]):
         cfg = {}
         if not dirs[0]:
-            dirs = [ '/usr', sys.prefix ] + glob.glob('/opt/libpcap*') + \
+            dirs = ['/usr', sys.prefix] + glob.glob('/opt/libpcap*') + \
                 glob.glob('../libpcap*') + glob.glob('../wpdpack*') + \
                 ['/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/',
                  '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*']
@@ -87,7 +94,7 @@ class config_pcap(config.config):
             if pcap_h:
                 print "Found pcap headers in %s" % pcap_h
 
-                cfg['include_dirs'] = [ os.path.dirname(pcap_h) ]
+                cfg['include_dirs'] = [os.path.dirname(pcap_h)]
                 lib_sub_dirs = [os.path.join(d, sub_dir) \
                         for sub_dir in ('lib', 'lib64', \
                         'lib/x86_64-linux-gnu', 'lib/i386-linux-gnu', '')]
@@ -104,14 +111,14 @@ class config_pcap(config.config):
 
                 lib_file = os.path.basename(lib_file_path)
 
-                cfg['library_dirs'] = [ os.path.dirname(lib_file_path) ]
-                cfg['libraries'] = [ lib_files[lib_file] ]
+                cfg['library_dirs'] = [os.path.dirname(lib_file_path)]
+                cfg['libraries'] = [lib_files[lib_file]]
 
                 #cfg['libraries'] = [ lib[0] ]
                 if lib_file == 'wpcap.lib':
                     cfg['libraries'].append('iphlpapi')
                     cfg['extra_compile_args'] = \
-                        [ '-DWIN32', '-DWPCAP' ]
+                        ['-DWIN32', '-DWPCAP']
 
                 self._write_config_h(cfg)
                 return cfg
@@ -120,7 +127,7 @@ class config_pcap(config.config):
 
     def run(self):
         #config.log.set_verbosity(0)
-        cPickle.dump(self._pcap_config([ self.with_pcap ]),
+        cPickle.dump(self._pcap_config([self.with_pcap]),
                      open(pcap_cache, 'wb'))
         self.temp_files.append(pcap_cache)
 
@@ -139,21 +146,27 @@ if len(sys.argv) > 1 and sys.argv[1] == 'build':
         print >>sys.stderr, 'run "%s config" first!' % sys.argv[0]
         sys.exit(1)
 
-pcap = Extension(name='pcap',
-                 sources=[ 'pcap.c', 'pcap_ex.c' ],
-                 include_dirs=pcap_config.get('include_dirs', ''),
-                 library_dirs=pcap_config.get('library_dirs', ''),
-                 libraries=pcap_config.get('libraries', ''),
-                 extra_compile_args=pcap_config.get('extra_compile_args', ''))
+pcap = Extension(
+    name='pcap',
+    sources=['pcap.c', 'pcap_ex.c'],
+    include_dirs=pcap_config.get('include_dirs', ''),
+    library_dirs=pcap_config.get('library_dirs', ''),
+    libraries=pcap_config.get('libraries', ''),
+    extra_compile_args=pcap_config.get('extra_compile_args', '')
+)
 
-pcap_cmds = { 'config':config_pcap, 'clean':clean_pcap }
+pcap_cmds = {
+    'config': config_pcap,
+    'clean': clean_pcap
+}
 
-setup(name='pcap',
-      version='1.1',
-      author='Dug Song',
-      author_email='dugsong@monkey.org',
-      url='http://monkey.org/~dugsong/pypcap/',
-      description='packet capture library',
-      cmdclass=pcap_cmds,
-      ext_modules = [ pcap ])
-
+setup(
+    name='pcap',
+    version='1.1',
+    author='Dug Song',
+    author_email='dugsong@monkey.org',
+    url='http://monkey.org/~dugsong/pypcap/',
+    description='packet capture library',
+    cmdclass=pcap_cmds,
+    ext_modules=[pcap]
+)
