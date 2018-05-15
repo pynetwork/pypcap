8761f0e6f2cd7ea573f2a95341a04a2e51d4fdc5
lambdalisue <lambdalisue@hashnote.net>
2018-05-15 23:57:22 +0900

    Refactoring setup.py
    1. Simplify/Beautify code
    2. itertools.chain.from_iterable exists from Python 2.6
    3. io.open is required in Python 2.x while builtin 'open' does not support 'encoding'
    4. the builtin 'open' is an alias of io.open in Python 3.x so using io.open in Python 3.x
       doesn't have effect
    
    Close #70

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 """Setup for pycapy"""
 from setuptools import setup, Extension
+from itertools import chain
+from io import open
 import glob
 import os
 import sys
-import re
 
 PACKAGE = "pypcap"
 VERSION = "1.2.1"
@@ -25,49 +26,51 @@ def recursive_search(path, target_files):
                 return os.path.join(root, filename)
 
 
-def get_extension():
-    # A list of all the possible search directories
-    dirs = ['/usr', sys.prefix] + glob.glob('/opt/libpcap*') + \
-        glob.glob('../libpcap*') + glob.glob('../wpdpack*') + \
-        glob.glob('/Applications/Xcode.app/Contents/Developer/Platforms/' +
-                  'MacOSX.platform/Developer/SDKs/*')
+def find_prefix_and_pcap_h():
+    prefixes = chain.from_iterable((
+        ('/usr', sys.prefix),
+        glob.glob('/opt/libpcap*'),
+        glob.glob('../libpcap*'),
+        glob.glob('../wpdpack*'),
+        glob.glob('/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*'),
+    ))
 
-    for d in dirs:
-        search_dirs = [
-            os.path.join(d, 'local', 'include'),
-            os.path.join(d, 'usr', 'include'),
-            os.path.join(d, 'include'),
-            d
-        ]
+    # Find 'pcap.h'
+    for prefix in prefixes:
+        search_dirs = (
+            os.path.join(prefix, 'local', 'include'),
+            os.path.join(prefix, 'usr', 'include'),
+            os.path.join(prefix, 'include'),
+            prefix,
+        )
 
         pcap_h = recursive_search_dirs(search_dirs, ['pcap.h'])
         if pcap_h:
             print("Found pcap headers in %s" % pcap_h)
-            break
+            return (prefix, pcap_h)
+    print("pcap.h not found")
+    sys.exit(1)
 
-    if not pcap_h:
-        print("pcap.h not found")
-        sys.exit(1)
 
-    include_dirs = [os.path.dirname(pcap_h)]
-
-    # This logic will use the path 'd' that the pcap.h was found in
-    is_64bits = sys.maxsize > 2**32
-    priority_libs = (
-        'lib64',
-        'lib/x64',  # wpdpack
-        'lib/x86_64-linux-gnu'
-    ) if is_64bits else tuple()
-
-    lib_sub_dirs = [
-        os.path.join(d, sub_dir)
-        for sub_dir in priority_libs + (
+def find_lib_path_and_file(prefix):
+    if sys.maxsize > 2 ** 32:
+        candidates = [
+            'lib64',
+            'lib/x64',  # wpdpack
+            'lib/x86_64-linux-gnu'
             'lib',
             'lib/i386-linux-gnu',
             ''
-        )
+        ]
+    else:
+        candidates = [
+            'lib',
+            'lib/i386-linux-gnu',
+            ''
+        ]
+    lib_sub_dirs = [
+        os.path.join(prefix, d) for d in candidates
     ]
-
     # For Mac OSX the default system pcap lib is in /usr/lib
     lib_sub_dirs.append('/usr/lib')
 
@@ -78,58 +81,66 @@ def get_extension():
         'wpcap.lib'
     ]
     lib_file_path = recursive_search_dirs(lib_sub_dirs, lib_files)
-
+    if not lib_file_path:
+        print("None of the following found: %s" % lib_files)
+        sys.exit(1)
+        return
     print("Found libraries in %s" % lib_file_path)
 
-    lib_file = os.path.basename(lib_file_path)
     lib_path = os.path.dirname(lib_file_path)
+    lib_file = os.path.basename(lib_file_path)
+    return lib_path, lib_file
 
-    extra_compile_args = []
-    if re.match(r"libpcap\.(a|so|dylib)", lib_file):
-        libraries = ('pcap',)
-    elif lib_file == "wpcap.lib":
-        libraries = ('wpcap', 'iphlpapi')
+
+def find_define_macros(pcap_h):
+    with open(pcap_h, 'r',
+              encoding='utf-8',
+              errors='surrogateescape') as fi:
+        for line in fi.readlines():
+            if 'pcap_file(' in line:
+                print("found pcap_file function")
+                yield ('HAVE_PCAP_FILE', 1)
+            elif 'pcap_compile_nopcap(' in line:
+                print("found pcap_compile_nopcap function")
+                yield ('HAVE_PCAP_COMPILE_NOPCAP', 1)
+            elif 'pcap_setnonblock(' in line:
+                print("found pcap_setnonblock")
+                yield ('HAVE_PCAP_SETNONBLOCK', 1)
+            elif 'pcap_setdirection(' in line:
+                print("found pcap_setdirection")
+                yield ('HAVE_PCAP_SETDIRECTION', 1)
+
+
+def get_extension():
+    prefix, pcap_h = find_prefix_and_pcap_h()
+    lib_path, lib_file = find_lib_path_and_file(prefix)
+
+    if lib_file == 'wpcap.lib':
+        libraries = ['wpcap', 'iphlpapi']
         extra_compile_args = ['-DWIN32', '-DWPCAP', '-D_CRT_SECURE_NO_WARNINGS']
+    else:
+        libraries = ['pcap']
+        extra_compile_args = []
 
-    define_macros = []
-
-    pcap_h_file = open(pcap_h).readlines()
-    for line in pcap_h_file:
-        if 'pcap_file(' in line:
-            print("found pcap_file function")
-            define_macros.append(('HAVE_PCAP_FILE', 1))
-        if 'pcap_compile_nopcap(' in line:
-            print("found pcap_compile_nopcap function")
-            define_macros.append(('HAVE_PCAP_COMPILE_NOPCAP', 1))
-        if 'pcap_setnonblock(' in line:
-            print("found pcap_setnonblock")
-            define_macros.append(('HAVE_PCAP_SETNONBLOCK', 1))
-        if 'pcap_setdirection(' in line:
-            print("found pcap_setdirection")
-            define_macros.append(('HAVE_PCAP_SETDIRECTION', 1))
-
-    ext = Extension(
+    return Extension(
         name='pcap',
         sources=['pcap.c', 'pcap_ex.c'],
-        include_dirs=include_dirs,
-        define_macros=define_macros,
-        library_dirs=[lib_path, ],
-        libraries=list(libraries),
+        include_dirs=[os.path.dirname(pcap_h)],
+        define_macros=list(find_define_macros(pcap_h)),
+        library_dirs=[lib_path],
+        libraries=libraries,
         extra_compile_args=extra_compile_args
     )
-    return ext
 
-setup_args = dict(
-    name=PACKAGE,
-    version=VERSION,
-    author='Dug Song',
-    author_email='dugsong@monkey.org',
-    url='https://github.com/pynetwork/pypcap',
-    description='pypcap -- Python interface to pcap a packet capture library',
-    tests_require=['dpkt']
-)
 
-if __name__ == "__main__":
-    ext = get_extension()
-    setup_args['ext_modules'] = [ext]
-    setup(**setup_args)
+if __name__ == '__main__':
+    setup(
+        name=PACKAGE,
+        version=VERSION,
+        author='Dug Song',
+        author_email='dugsong@monkey.org',
+        url='https://github.com/pynetwork/pypcap',
+        description='pypcap -- Python interface to pcap a packet capture library',
+        tests_require=['dpkt'],
+        ext_modules=[get_extension()],
+    )
